262499cacc691a51be01fabb1086851c1eaa60b8
mqjunioretecgv@gmail.com <mqjunioretecgv@gmail.com>
2022-05-09 11:54:43 -0300

    did it work?

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+
+
+var mountNode = document.getElementById("root");
+render(<App name="Jane" />, mountNode);
