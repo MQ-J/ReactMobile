4b058ff638a0f77f542b966885355aa9789a1710
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-17 19:41:57 +0000

    1.7.4 - please come back

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: "/ReactMobile/dist"
+    publicPath: '/'
   },
   module: {
     rules: [
