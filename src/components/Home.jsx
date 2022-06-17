a124eb46c23337bae0b0207881f12234f2558653
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-17 12:13:55 +0000

    1.5.3 - fix user modal change user modal to navbar component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,50 +8,6 @@ export default function Home() {
   return (
     <div>
 
-      {/*  Modal de usuário  */}
-      <div
-        className="modal fade"
-        id="modal"
-        tabIndex="-1"
-        aria-labelledby="modalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="modalLabel">
-                Você mesmo
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="modal-body">
-              <p>Seus dados de usuário</p>
-              <p>Demais informações</p>
-              <p>insira EasterEgg aqui</p>
-            </div>
-
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Deslogar
-              </button>
-              <button type="button" className="btn btn-primary">
-                ok
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <Navbar />
 
       {/* conteúdo */}
