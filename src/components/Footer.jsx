cb66e8001706f04ce964ed1d677fa91b411d031a
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-06-17 13:59:11 +0000

    modal  and footer colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ export function Footer() {
         <div className="d-flex justify-content-around border border-dark rounded fixed-bottom m-3 bg-cadet">
             <div className="d-flex flex-column justify-content-center w-25">
                 <a className="btn p-0" href="https://getbootstrap.com/docs/5.0/getting-started/introduction/" role="button">
-                    <p className="text-white text-center text-nowrap mb-0">Bootstrap 5</p>
+                    <p className="text-center text-nowrap mb-0">Bootstrap 5</p>
                 </a>
             </div>
 
             <div className="d-flex flex-column justify-content-center w-25">
                 <a className="btn p-0" href="https://pt-br.reactjs.org/" role="button">
-                    <p className="text-white text-center mb-0">React</p>
+                    <p className="text-center mb-0">React</p>
                 </a>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,85 +5,86 @@ export function Navbar() {
 
     return (
         <>
-        {/*  Modal de usuário  */}
-        <div
-            className="modal fade"
-            id="modal"
-            tabIndex="-1"
-            aria-labelledby="modalLabel"
-            aria-hidden="true"
-        >
-            <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-                <div className="modal-header">
-                <h5 className="modal-title" id="modalLabel">
-                    Você mesmo
-                </h5>
-                <button
-                    type="button"
-                    className="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                ></button>
-                </div>
-
-                <div className="modal-body">
-                <p>Seus dados de usuário</p>
-                <p>Demais informações</p>
-                <p>insira EasterEgg aqui</p>
-                </div>
-
-                <div className="modal-footer">
-                <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-dismiss="modal"
-                >
-                    Deslogar
-                </button>
-                <button type="button" className="btn btn-primary">
-                    ok
-                </button>
-                </div>
-            </div>
-            </div>
-        </div>
-
-        {/* Barra de navegação */ }
-        < nav className = "navbar justify-content-around mb-2 bg-cadet" >
-
-            {/* icone de usuário */ }
-            < button
-            className = "btn p-2"
-            data-bs-toggle="modal"
-            data-bs-target="#modal"
+            {/*  Modal de usuário  */}
+            <div
+                className="modal fade"
+                id="modal"
+                tabIndex="-1"
+                aria-labelledby="modalLabel"
+                aria-hidden="true"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="currentColor"
-                    className="bi bi-person-circle"
-                    viewBox="0 0 16 16"
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content bg-gunmetal">
+
+                        <div className="modal-header bg-cadet border-3 border-dark">
+                            <h5 className="modal-title" id="modalLabel">
+                                Você mesmo
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+
+                        <div className="modal-body">
+                            <p>Seus dados de usuário</p>
+                            <p>Demais informações</p>
+                            <p>insira EasterEgg aqui</p>
+                        </div>
+
+                        <div className="modal-footer border-3 border-dark">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Deslogar
+                            </button>
+                            <button type="button" className="btn text-white bg-orange">
+                                ok
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Barra de navegação */}
+            < nav className="navbar justify-content-around mb-2 bg-cadet" >
+
+                {/* icone de usuário */}
+                < button
+                    className="btn p-2"
+                    data-bs-toggle="modal"
+                    data-bs-target="#modal"
                 >
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                    <path
-                        fillRule="evenodd"
-                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                    />
-                </svg>
-            </button >
-      
-            <Link className="nav-link link-dark p-2" to="/Home" >
-            Home
-            </Link>
-            <Link className="nav-link link-dark p-2" to="/Respect" >
-                Respeito aos pais
-            </Link>
-            <Link className="nav-link link-dark p-2" to="/StarWars" >
-                StarWars
-            </Link>
-        </nav >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        fill="currentColor"
+                        className="bi bi-person-circle"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path
+                            fillRule="evenodd"
+                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                        />
+                    </svg>
+                </button >
+
+                <Link className="nav-link link-dark p-2" to="/Home" >
+                    Home
+                </Link>
+                <Link className="nav-link link-dark p-2" to="/Respect" >
+                    Respeito aos pais
+                </Link>
+                <Link className="nav-link link-dark p-2" to="/StarWars" >
+                    StarWars
+                </Link>
+            </nav >
         </>
     );
 }
