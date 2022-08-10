0d6c2dc3b91260c2012a082d029d564e2669598f
MQ-J <mqjunioretecgv@gmail.com>
2022-08-10 11:50:25 -0300

    1.13.1_dev + msg que confirma apagar conta + msg que confirma apagar menu + e claro, agr pode apagar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,26 +41,27 @@ export function Navbar(props) {
         var formData = new FormData();
         formData.append('name', props.user);
 
-        fetch(
-            `${host}/api/ReactMobile/deleteUser`,
-            {
-                body: new URLSearchParams(formData),
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-                method: "post",
-            }
-        ).then((res) => res.json()).then((res) => {
+        if (confirm("deseja mesmo apagar sua conta, incluindo TODAS as suas anotações ?")) {
+            fetch(
+                `${host}/api/ReactMobile/deleteUser`,
+                {
+                    body: new URLSearchParams(formData),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    method: "post",
+                }
+            ).then((res) => res.json()).then((res) => {
 
-            if (res['status'] == 'ok') {
-                alert("tchau")
-                removeLogin()
+                if (res['status'] == 'ok') {
+                    removeLogin()
 
-            } else {
-                alert(res['message']) // aqui se trata todas as respostas Nok da API
+                } else {
+                    alert(res['message']) // aqui se trata todas as respostas Nok da API
+                }
             }
+            );
         }
-        );
     }
 
     // cria menu
@@ -95,8 +96,35 @@ export function Navbar(props) {
     };
 
     // apaga menu
-    const deleteMenu = (menu) => {
-        alert("apaga o tópico de " + menu)
+    const deleteMenu = (menu, code, user) => {
+        if (confirm("quer mesmo apagar o menu " + menu + "?")) {
+
+            const url = process.env.NODE_ENV == "development" ? "http://127.0.0.1:8000" : "https://polar-shelf-77439.herokuapp.com"
+
+            var formData = new FormData()
+            formData.append('name', user)
+            formData.append('code', code)
+
+            fetch(
+                `${url}/api/ReactMobile/deleteMenu`,
+                {
+                    body: new URLSearchParams(formData),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    method: "post",
+                }
+            ).then((res) => res.json()).then((res) => {
+
+                if (res['status'] == 'ok') {
+                    removeLogin()
+                } else {
+                    setLoading(false)
+                    alert(res['message'])
+                }
+            }
+            );
+        }
     }
 
     return (
@@ -353,7 +381,7 @@ export function Navbar(props) {
                                                             <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
                                                         </svg>
                                                     </button>
-                                                    <button className="btn btn-link border border-dark" onClick={() => deleteMenu(menu)} type="button" id={"alter" + menu}>
+                                                    <button className="btn btn-link border border-dark" onClick={() => deleteMenu(menu, code, props.user)} type="button" id={"alter" + menu}>
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-dash-circle text-danger ms-2" viewBox="0 0 16 16">
                                                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                                             <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
